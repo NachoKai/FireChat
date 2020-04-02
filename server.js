2b27f3f27572d0a7f9fa9569f8f22c58492ee5ff
Nacho Caiafa <nachokai@outlook.com>
2020-04-02 19:34:45 -0300

    Commit message

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ let serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
+router.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 let port = process.env.PORT || 8000;
 app.listen(port);
 console.log('server started ' + port);
